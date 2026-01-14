2551155a0a1a27359af61ab56d8ed8d5b7d41a70
Mendel <44873735+rabbidiesel@users.noreply.github.com>
2026-01-14 12:27:49 -0500

    Moved destination to the front

--- bulk_account_settings.php
+++ bulk_account_settings.php
@@ -46,11 +46,11 @@
 	echo "<div class='action_bar' id='action_bar'>\n";
 	echo "	<div class='heading'><b>".$text['header-bulk_account_settings']."</b></div>\n";
 	echo "	<div class='actions'>\n";
+	echo "<a href='bulk_account_settings_destinations.php'>".button::create(['type'=>'button','label'=>$text['button-destinations'],'icon'=>'location-arrow'])."</a>";
 	echo "<a href='bulk_account_settings_devices.php'>".button::create(['type'=>'button','label'=>$text['button-devices'],'icon'=>'mobile-retro'])."</a>";
 	echo "<a href='bulk_account_settings_extensions.php'>".button::create(['type'=>'button','label'=>$text['button-extensions'],'icon'=>'suitcase'])."</a>";
 	echo "<a href='bulk_account_settings_users.php'>".button::create(['type'=>'button','label'=>$text['button-users'],'icon'=>'user-group'])."</a>";
 	echo "<a href='bulk_account_settings_voicemails.php'>".button::create(['type'=>'button','label'=>$text['button-voicemails'],'icon'=>'envelope'])."</a>";
-	echo "<a href='bulk_account_settings_destinations.php'>".button::create(['type'=>'button','label'=>$text['button-destinations'],'icon'=>'location-arrow'])."</a>";
 	// echo "<a href='bulk_account_settings_call_routing.php'>".button::create(['type'=>'button','label'=>$text['button-call_routing']])."</a>";
 	echo "	</div>\n";
 	echo "	<div style='clear: both;'></div>\n";
