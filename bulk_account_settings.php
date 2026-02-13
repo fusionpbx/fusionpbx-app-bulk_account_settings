3d8e15669710331e57f564c1d5c1ad7061f2d877
Alex <40072887+alexdcrane@users.noreply.github.com>
2026-02-13 13:48:15 -0700

    Update bulk_account_settings.php

--- bulk_account_settings.php
+++ bulk_account_settings.php
@@ -17,7 +17,7 @@
 
 	The Initial Developer of the Original Code is
 	Mark J Crane <markjcrane@fusionpbx.com>
-	Portions created by the Initial Developer are Copyright (C) 2008-2025
+	Portions created by the Initial Developer are Copyright (C) 2008-2026
 	the Initial Developer. All Rights Reserved.
 
 	Contributor(s):
@@ -49,6 +49,7 @@
 	echo "<a href='bulk_account_settings_destinations.php'>".button::create(['type'=>'button','label'=>$text['button-destinations'],'icon'=>'location-arrow'])."</a>";
 	echo "<a href='bulk_account_settings_devices.php'>".button::create(['type'=>'button','label'=>$text['button-devices'],'icon'=>'mobile-retro'])."</a>";
 	echo "<a href='bulk_account_settings_extensions.php'>".button::create(['type'=>'button','label'=>$text['button-extensions'],'icon'=>'suitcase'])."</a>";
+	echo "<a href='bulk_account_settings_gateways.php'>".button::create(['type'=>'button','label'=>$text['button-gateways'],'icon'=>'globe'])."</a>";
 	echo "<a href='bulk_account_settings_users.php'>".button::create(['type'=>'button','label'=>$text['button-users'],'icon'=>'user-group'])."</a>";
 	echo "<a href='bulk_account_settings_voicemails.php'>".button::create(['type'=>'button','label'=>$text['button-voicemails'],'icon'=>'envelope'])."</a>";
 	// echo "<a href='bulk_account_settings_call_routing.php'>".button::create(['type'=>'button','label'=>$text['button-call_routing']])."</a>";
